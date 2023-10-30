c24a1a567bdc1977c6c92ce6289084660567ec61
Elliot <elliotc223@gmail.com>
2023-10-30 15:24:24 +1000

    Home page Styling

--- app/src/main/java/com/iab330/weatheralert/MainActivity.java
+++ app/src/main/java/com/iab330/weatheralert/MainActivity.java
@@ -6,12 +6,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.iab330.weatheralert.DB.TemperatureData;
@@ -25,28 +24,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     TextView dataLink;
     Intent service;
     SensorDataReceiver dataReceiver;
-    SensorManager sensorManager;
+    LinearLayout mainLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         setViewIds();
+        handleBackgroundChange();
         handleAlertClick();
         handleSettingClick();
         handleHomeClick();
-        handleDataClick();
         manageSensorService();
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
     }
 
     private class SensorDataReceiver extends BroadcastReceiver {
@@ -79,19 +69,59 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
 
-    private void handleDataClick(){
-        dataLink.setOnClickListener(view -> {
-            Intent intent = new Intent(this, DataActivity.class);
-            startActivity(intent);
-        });
-    }
-
     private void handleHomeClick(){
         btnHome.setOnClickListener(view -> {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         });
     }
+
+    private void handleBackgroundChange() {
+        Calendar c = Calendar.getInstance();
+        int timeOfDay = c.get(Calendar.HOUR_OF_DAY);
+
+        TextView timeOfDay2 = findViewById(R.id.timeOfDay);
+
+
+        if(timeOfDay >= 8 && timeOfDay < 16){
+            mainLayout.setBackgroundResource(R.drawable.background_2);
+            setColour("Midday");
+            timeOfDay2.setText("Midday,");
+        }else if(timeOfDay >= 16 && timeOfDay < 20){
+            mainLayout.setBackgroundResource(R.drawable.background_3);
+            setColour("Evening");
+            timeOfDay2.setText("Evening,");
+        }else if(timeOfDay >= 0 && timeOfDay < 4){
+            mainLayout.setBackgroundResource(R.drawable.background_4);
+            setColour("Night");
+            timeOfDay2.setText("Night,");
+        }else if(timeOfDay >= 4 && timeOfDay < 8){
+            mainLayout.setBackgroundResource(R.drawable.background_1);
+            setColour("Dawn");
+            timeOfDay2.setText("Dawn,");
+        }
+    }
+
+    private void setColour(String timeframe) {
+        LinearLayout mainLayout = findViewById(R.id.mainLayout);
+        String currentColor;
+
+        if(Objects.equals(timeframe, "Midday") || Objects.equals(timeframe, "Evening")) {
+            currentColor = "#373D3F";
+        } else if(Objects.equals(timeframe, "Night")) {
+            currentColor = "#D9D9D9";
+        } else {
+            currentColor = "F2F3F4";
+        }
+
+        for (int i = 0; i < mainLayout.getChildCount(); i++) {
+            View v = mainLayout.getChildAt(i);
+            if (v instanceof TextView) {
+                ((TextView) v).setTextColor(Color.parseColor(currentColor));
+            }
+        }
+    }
+
     private void handleAlertClick() {
         btnAlert.setOnClickListener(view -> {
             Intent intent = new Intent(this, AlertActivity.class);
@@ -108,6 +138,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         btnHome = findViewById(R.id.homeBtn);
         btnSetting = findViewById(R.id.settingsBtn);
         btnAlert = findViewById(R.id.alertBtn);
-        dataLink = findViewById(R.id.detailsText);
+        mainLayout = findViewById(R.id.mainLayout);
     }
 }
