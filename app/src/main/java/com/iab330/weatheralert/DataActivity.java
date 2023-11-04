befbc4a3fb6c8deb7954a5ebff6e5535e2acc78f
elshant <elshanrt@gmail.com>
2023-11-04 12:41:35 +1000

    Updated Sensor Data Chart (Progress bar + better readability)

--- app/src/main/java/com/iab330/weatheralert/DataActivity.java
+++ app/src/main/java/com/iab330/weatheralert/DataActivity.java
@@ -8,7 +8,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 
 import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
@@ -38,6 +40,8 @@ public class DataActivity extends AppCompatActivity {
     private ImageButton btnHome;
     private ImageButton btnAlert;
     private ImageButton btnSetting;
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +76,8 @@ public class DataActivity extends AppCompatActivity {
         btnHome = findViewById(R.id.homeBtn);
         btnSetting = findViewById(R.id.settingsBtn);
         btnAlert = findViewById(R.id.alertBtn);
+        progressBar = findViewById(R.id.progress_bar);
+
     }
 
     private void analyseTemperatureData() {
@@ -80,8 +86,9 @@ public class DataActivity extends AppCompatActivity {
         AnyChartView tempChartView = findViewById(R.id.tempChart);
 
         Cartesian tempLineChart = AnyChart.line();
-        tempLineChart.animation(true); // Setting Animation for the chart
-        tempLineChart.padding(10d, 20d, 5d, 20d);
+        tempLineChart.animation(true);
+
+        tempLineChart.padding(20d, 30d, 20d, 30d);
 
         tempLineChart.crosshair().enabled(true);
         tempLineChart.crosshair()
@@ -91,41 +98,50 @@ public class DataActivity extends AppCompatActivity {
         tempLineChart.tooltip().positionMode(TooltipPositionMode.POINT);
 
         tempLineChart.title("Temperature analysis over the time.");
-        tempLineChart.yAxis(0).title("Temperature (Degree celcius)");
+        tempLineChart.title().fontSize(20d);
+
+        tempLineChart.yAxis(0).title("Temperature (°C)");
+        tempLineChart.yAxis(0).labels().fontSize(15d); // increased y-axis labels
+
         tempLineChart.xAxis(0).title("Timestamp");
-        tempLineChart.xAxis(0).labels().padding(5d, 5d, 5d, 5d);
+        tempLineChart.xAxis(0).labels().fontSize(15d).padding(10d, 10d, 10d, 10d); // Increased x-axis
+
+
         tempLineChart.legend().enabled(true);
-        tempLineChart.legend().fontSize(13d);
-        tempLineChart.legend().padding(0d, 0d, 10d, 0d);
+        tempLineChart.legend().fontSize(15d);
+        tempLineChart.legend().padding(0d, 0d, 20d, 0d);
+
 
         List<DataEntry> seriesData = new ArrayList<>();
-        Set set = Set.instantiate();
-        Mapping series1Mapping = set.mapAs("{ x: 'x', value: 'value' }");
+            Set set = Set.instantiate();
+            Mapping series1Mapping = set.mapAs("{ x: 'x', value: 'value' }");
 
-        Line series1 = tempLineChart.line(series1Mapping);
-        series1.name("Temperature Change");
-        series1.hovered().markers().enabled(true);
-        series1.hovered().markers()
-                .type(MarkerType.CIRCLE)
-                .size(4d);
-        series1.tooltip()
-                .position("right")
-                .anchor(Anchor.LEFT_CENTER)
-                .offsetX(5d)
-                .offsetY(5d);
+            Line series1 = tempLineChart.line(series1Mapping);
+            series1.name("Temperature Change");
+            series1.hovered().markers().enabled(true);
+            series1.hovered().markers()
+                    .type(MarkerType.CIRCLE)
+                    .size(4d);
+            series1.tooltip()
+                    .position("right")
+                    .anchor(Anchor.LEFT_CENTER)
+                    .offsetX(5d)
+                    .offsetY(5d);
 
-        tempChartView.setChart(tempLineChart); // adding the chart to the chart view
+            tempChartView.setChart(tempLineChart);
+            TemperatureDao temperatureDao = MyApp.getAppDatabase().temperatureDao();
+            LiveData<List<TemperatureData>> temperatureLiveData = temperatureDao.getAllTemperatureData();
+            temperatureLiveData.observe(this, temperatureDataList -> {
+                seriesData.clear();
+                for(TemperatureData temperatureData: temperatureDataList){
+                    Date date = new Date(temperatureData.getTimeStamp());
+                    seriesData.add(new CustomDataEntry(dateFormat.format(date), temperatureData.getTemp()));
+                }
+                set.data(seriesData);
+                tempChartView.invalidate();
+                progressBar.setVisibility(View.GONE); // hiding the ProgressBar
+            });
 
-        TemperatureDao temperatureDao = MyApp.getAppDatabase().temperatureDao();
-        LiveData<List<TemperatureData>> temperatureLiveData = temperatureDao.getAllTemperatureData();
-        temperatureLiveData.observe(this, temperatureDataList -> {
-            for(TemperatureData temperatureData: temperatureDataList){
-                Date date = new Date(temperatureData.getTimeStamp());
-                seriesData.add(new CustomDataEntry(dateFormat.format(date), temperatureData.getTemp()));
-            }
-            set.data(seriesData);
-
-        });
     }
 
     private class CustomDataEntry extends ValueDataEntry {
